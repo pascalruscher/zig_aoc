3f422cb503f909ef16b27a8400d0fe0053b51258
p0d4r14n <p0d4r14n@protonmail.com>
2023-05-02 16:32:17 +0200

    day04a solution

--- 2015/day04/solution_a.zig
+++ 2015/day04/solution_a.zig
@@ -2,23 +2,40 @@ const std = @import("std");
 const Md5 = std.crypto.hash.Md5;
 const mem = std.mem;
 const fmt = std.fmt;
-const expect = std.testing.expect;
+const heap = std.heap;
 
-pub fn main() void {
+pub fn main() !void {
     const input = "yzbqklnj";
-    const xy: u32 = 2;
-    const testd = [_]u32{xy};
-    const testd2 = input ++ testd;
-    std.log.debug("{}", testd2);
-    var hashed = [_]u8{0} ** Md5.digest_length;
 
-    var i: u32 = 1;
-    var found = false;
-    while (!found) : (i += 1) {
-        const is = [_]u32{i};
-        const testx = input ++ is;
-        Md5.hash(testx, &hashed, .{});
-        found = mem.bytesAsValue(u32, hashed[0..4]).* == 0;
+    std.debug.print("Solution: {!d}", .{getSecretAnswer(input)});
+}
+
+pub fn getSecretAnswer(input: []const u8) !usize {
+    var hashed = [_]u8{1} ** Md5.digest_length;
+
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var i: usize = 1;
+    while (true) : (i += 1) {
+        const concat = try fmt.allocPrint(allocator, "{s}{d}", .{ input, i });
+
+        Md5.hash(concat, &hashed, .{});
+        if (mem.eql(u8, fmt.bytesToHex(hashed, fmt.Case.lower)[0..5], "00000")) {
+            break;
+        }
     }
-    std.log.debug("Solution: {d}", .{i});
+
+    return i;
+}
+
+test "getSecretAnswer for abcdef" {
+    const secret = try getSecretAnswer("abcdef");
+    try std.testing.expect(secret == 609043);
+}
+
+test "getSecretAnswer for pqrstuv" {
+    const secret = try getSecretAnswer("pqrstuv");
+    try std.testing.expect(secret == 1048970);
 }
